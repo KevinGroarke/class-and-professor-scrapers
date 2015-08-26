1b3d8da49767e45355988b6b57030b37f234e24b
Kevin Groarke <kgroarke@ucsd.edu>
2015-08-25 21:04:57 -0700

    Cape scraper works perfectly, well need to go to json format

--- capeScraper.py
+++ capeScraper.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import scrapy
 import time
+
 
 class CapeSpider(scrapy.Spider):
     name = 'capeSpider'
@@ -12,81 +16,51 @@ class CapeSpider(scrapy.Spider):
 
     def parse(self, response):
         self.driver.get(response.url)
+        # Navigate to evaluation results
         self.driver.find_element_by_xpath('//*[@id="tdr_2_col_content"]/ul/li[2]/a').click()
-        options = self.driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_ddlDepartments"]').find_elements_by_tag_name('option')
+        options = self.driver.find_element_by_xpath(
+            '//*[@id="ctl00_ContentPlaceHolder1_ddlDepartments"]').find_elements_by_tag_name('option')
         options.pop(0)
         submit = self.driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_btnSubmit"]')
+        cape_courses = []
 
+        # Navigate each subject's capes
         for option in options:
             option.click()
-            self.driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_btnSubmit"]').click()
 
-            time.sleep(5)
+            # Sometimes it gave me an error that the submit button wasn't attached to the DOM...So just till it is
+            try:
+                WebDriverWait(self.driver, 60).until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_btnSubmit"]'))
+                    )
+                WebDriverWait(self.driver, 60).until(
+                    EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_btnSubmit"]'))
+                    )
+                self.driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_btnSubmit"]').click()
+            except ValueError:
+                self.data.write('couldn\'t click an option\n')
+
+            # Wait based on the red spinning thing disappearing (it's loading results)
+            time.sleep(1)
+            try:
+                WebDriverWait(self.driver, 60).until(
+                    EC.invisibility_of_element_located((By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_UpdateProgress1"]/div/div/img'))
+                    )
+            except ValueError:
+                self.data.write('sum error\n')
+
             sel = scrapy.Selector(text=self.driver.page_source.encode('utf-8'))
-            def encode_text(element): 
-                return element.encode('utf-8')
-            instructors = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[1]/text()').extract())
-            courses = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[2]/a/text()').extract())
-            term = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[3]/text()').extract())
-            enroll = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[4]/text()').extract())
-            evals_made = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[5]/span/text()').extract())
-            rcmnd_class = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[6]/span/text()').extract())
-            rcmnd_instr = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[7]/span/text()').extract())
-            study_hrswk = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[8]/span/text()').extract())
-            avg_grade_exp = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[9]/span/text()').extract())
-            avg_grade_rec = map(encode_text, sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr/td[10]/span/text()').extract())
+            course_selectors = sel.xpath('//*[@id="ctl00_ContentPlaceHolder1_gvCAPEs"]/tbody/tr')
 
-            reports = zip(instructors, courses, term, enroll, evals_made, rcmnd_instr, study_hrswk, avg_grade_exp, avg_grade_rec)
-            
-            #self.data.write(str(len(instructors)) + ' ' + str(len(courses)) + ' ' + str(len(term)) + ' ' + str(len(enroll)) + ' ' + str(len(evals_made)) + ' ' + str(len(rcmnd_instr)) + ' ' + str(len(study_hrswk)) + ' ' + str(len(avg_grade_exp)) + ' ' + str(len(avg_grade_rec)))
-            for instructor in instructors:
-                self.data.write(instructor)
+            for course_selector in course_selectors:
+                professor_name = course_selector.xpath('.//td[1]/text()').extract_first(default='not found')
+                course_name = course_selector.xpath('.//td[2]/a/text()').extract_first(default='not found')
+                term = course_selector.xpath('.//td[3]/text()').extract_first(default='not found')
+                recommend_class = course_selector.xpath('.//td[6]/span/text()').extract_first(default='not found')
+                recommend_instructor = course_selector.xpath('.//td[7]/span/text()').extract_first(default='not found')
+                study_hours = course_selector.xpath('.//td[8]/span/text()').extract_first(default='not found')
+                avg_grade_exp = course_selector.xpath('.//td[9]/span/text()').extract_first(default='not found')
+                avg_grade_rec = course_selector.xpath('.//td[10]/span/text()').extract_first(default='not found')
 
-            self.data.write('\nCOURSES NOW!')
-
-            for course in courses:
-                self.data.write(course)
-
-            self.data.write('\nTERMS NOW!')
-
-            for element in term:
-                self.data.write(element)
-
-            self.data.write('\nENROLL NOW!')
-
-            for element in enroll:
-                self.data.write(element)
-
-            self.data.write('\nEvals made')
-
-            for element in evals_made:
-                self.data.write(element)
-
-            self.data.write('\nrcmnd_class')
-
-            for element in rcmnd_class:
-                self.data.write(element)
-
-            self.data.write('\nrcmnd_instr')
-
-            for element in rcmnd_instr:
-                self.data.write(element)
-
-            self.data.write('\nstudy_hrswk')
-
-            for element in study_hrswk:
-                self.data.write(element)
-
-            self.data.write('\navg_grade_exp')
-
-            for element in avg_grade_exp:
-                self.data.write(element)
-
-            self.data.write('\navg_grade_rec')
-
-            for element in avg_grade_rec:
-                self.data.write(element)
-            
-            #for report in reports:
-                #self.data.write(report[0] + ' ' + report[1] + ' ' + report[2] + ' ' + report[3] + ' ' + report[4] + ' ' + report[5] + ' ' + report[6] + ' ' + report[7] + ' ' + report[8])
-
+                self.data.write(professor_name + ' ' + course_name + ' ' + term + ' ' + recommend_class + ' ' +
+                    recommend_instructor + ' ' + study_hours + ' ' + avg_grade_exp + ' ' + avg_grade_rec + '\n')
